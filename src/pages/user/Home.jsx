e0f4302e018071605f9d89cdf9b2645054b11738
Jamshad <123630640+Jamshad10@users.noreply.github.com>
2023-10-09 15:01:33 +0530

    add button added in home page

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -92,6 +92,7 @@ const Home = () => {
                             />
                         </svg>
                     </div>
+                    <Link to={'/add'}>
                     <button className="bg-green-600 hover:bg-green-700 text-gray-50 font-bold py-2 px-4 rounded inline-flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -99,6 +100,7 @@ const Home = () => {
 
                         <span>Add New</span>
                     </button>
+                    </Link>
                 </div>
 
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg rounded-lg ">
